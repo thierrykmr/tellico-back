08463e40c722b3dce8c8af84b571c29a2def098f
tchemana <thierry-valtere@chemanack-leuma@etu.utc.fr>
2025-07-11 01:41:40 +0200

    Ajout Validation de Dtos et middleware pour analyse de cookies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());//pour analyser les cookies presents dans les requetes HTTP entrantes
+  
+  // Pour la securisation et validation automatique des DTOs (entrantes dans l'API) 
+  app.useGlobalPipes(new ValidationPipe())
+  useContainer(app.select(AppModule), { fallbackOnErrors: true })
+
   await app.listen(process.env.PORT ?? 3009);
 }
 bootstrap();
